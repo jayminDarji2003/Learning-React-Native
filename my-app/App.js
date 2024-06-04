852d36006310b5709f318b2e258f7db2efc73808
Jaymin Darji <jaymindarji2003@gmail.com>
2024-06-04 16:03:15 +0530

    started new technology called React Native

---
+++ my-app/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "red" }}>Hii i'm jaymin darji from gandhinagar gujarat</Text>
+      <StatusBar style="auto" />
+      <Button title='Press' onPress={() => console.log("clicked on button")} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
