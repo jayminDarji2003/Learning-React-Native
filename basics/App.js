61c0df202b150b2153108bf9e68c861dfdc225c4
Jaymin Darji <jaymindarji2003@gmail.com>
2024-06-04 16:30:35 +0530

    some changes on installation

--- basics/App.js
+++ basics/App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "red" }}>Hii i'm jaymin darji from gandhinagar gujarat</Text>
+      <Text>Hello world!!!</Text>
       <StatusBar style="auto" />
-      <Button title='Press' onPress={() => console.log("clicked on button")} />
     </View>
   );
 }
