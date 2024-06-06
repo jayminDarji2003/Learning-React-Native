512121ec7980d8a04e5adcd01e09343b68c09f4e
Jaymin Darji <jaymindarji2003@gmail.com>
2024-06-06 07:41:50 +0530

    learning new things

--- basics-2/fullstack/App.js
+++ basics-2/fullstack/App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, Platform, FlatList } from 'react-native';
+import userData from "./constants/userData.json"
 
 export default function App() {
+  // console.log(userData);
   return (
+    // <ScrollView>
+    //   <SafeAreaView>
+    //     <View style={styles.container}>
+    //       {
+    //         userData.map((user) => {
+    //           return <View key={user.id} style={styles.card}>
+    //             <Text style={styles.name}>{user.name}</Text>
+    //             <Text style={styles.gender}>{user.gender}</Text>
+    //             <Text style={styles.rofession}>{user.profession}</Text>
+    //           </View>
+    //         })
+    //       }
+    //       <StatusBar style="auto" />
+    //     </View>
+    //   </SafeAreaView>
+    // </ScrollView>
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={userData}
+        renderItem={({ item }) => {
+          return <View key={item.id} style={styles.card}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.gender}>{item.gender}</Text>
+            <Text style={styles.rofession}>{item.profession}</Text>
+          </View>
+        }}
+      // horizontal={true}
+      // keyExtractor={(item)=> item.toString()}
+      />
+
     </View>
   );
 }
@@ -16,5 +46,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 30,
+    backgroundColor: "#F0EBE3"
   },
+  card: {
+    borderWidth: 2,
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20,
+    marginVertical: 10,
+    backgroundColor: "#CDE8E5",
+    elevation: 20,
+    borderRadius: 10,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: "bold",
+
+  },
+  gender: {
+    fontWeight: "bold"
+  }
 });
