c813c061a79b40da9ee4f9ab4d3da42152cf3974
Jaymin Darji <jaymindarji2003@gmail.com>
2024-06-17 18:43:46 +0530

    working on this application

--- video-streaming-app/server/controllers/videoController.js
+++ video-streaming-app/server/controllers/videoController.js
@@ -1,43 +1,162 @@
-// controllers/videoUpload.js
-const cloudinary = require('../config/cloudinary');
-const Video = require('../models/Video');
+// importing models
+const cloudinary = require("cloudinary").v2
+const Video = require("../models/video")
 
-const videoUpload = async (req, res) => {
+// check file exists or not
+function isFileTypeSupported(type, supportedTypes) {
+    return supportedTypes.includes(type);
+}
+
+// video upload helper functions
+async function videoUploadFileToCloudinary(file, folder) {
+    const options = { folder, resource_type: 'video' };
     try {
-        const { title, prompt } = req.body;
-
-        // Check if files are present
-        if (!req.files || !req.files.video || !req.files.thumbnail) {
-            return res.status(400).json({ error: 'All fields are required' });
-        }
-
-        const { video, thumbnail } = req.files;
-
-        // Upload video to Cloudinary
-        const videoUploadResponse = await cloudinary.uploader.upload(video[0].path, {
-            resource_type: 'video',
-        });
-
-        // Upload thumbnail to Cloudinary
-        const thumbnailUploadResponse = await cloudinary.uploader.upload(thumbnail[0].path, {
-            resource_type: 'image',
-        });
-
-        // Save to database
-        const newVideo = new Video({
-            title,
-            videoUrl: videoUploadResponse.secure_url,
-            thumbnailUrl: thumbnailUploadResponse.secure_url,
-            prompt,
-        });
-
-        await newVideo.save();
-
-        res.status(201).json({ message: 'Video uploaded successfully', video: newVideo });
+        const result = await cloudinary.uploader.upload(file.tempFilePath, options);
+        console.log(result);
+        return result;
     } catch (error) {
+        console.log("ERROR IN FILE UPLOAD TO Cloudinary");
         console.error(error);
-        res.status(500).json({ error: 'Server error' });
+        throw error;
     }
-};
+}
 
-module.exports = videoUpload;
+// image upload helper functions
+async function imageUploadFileToCloudinary(file, folder) {
+    const options = { folder };
+    try {
+        const result = await cloudinary.uploader.upload(file.tempFilePath, options);
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.log("ERROR IN FILE UPLOAD TO Cloudinary");
+        console.error(error);
+        throw error;
+    }
+}
+
+
+// image upload
+const videoFileUpload = async (req, res) => {
+    try {
+        // fetch the data from req
+        const { title, prompt, user } = req.body;
+        //console.log("GET THE DATA : ", title, prompt, user);
+
+        // get file
+        const file = req.files.imageFile;
+        //console.log("GET IMAGE FILE : ", file);
+
+        // video file data
+        const videoFile = req.files.video;
+        //console.log("VIDEO FILE => ", videoFile)
+
+        // validation
+        // const supportedTypes = ["jpg", "jpeg", "png"];
+
+        // current file type
+        // const currentFileType = file.name.split(".")[1].toLowerCase();
+        // console.log("FILE EXTENSION : ", currentFileType);
+
+        // file type not supported
+        // if (!isFileTypeSupported(currentFileType, supportedTypes)) {
+        //     return res.status(400).json({
+        //         success: false,
+        //         message: "File type not supported"
+        //     })
+        // }
+
+        // if file type is supported
+        // upload file to cloudinary
+        console.log("UPLOADING TO CLOUDINARY")
+        const responseImage = await imageUploadFileToCloudinary(file, "aora");
+        console.log("IMAGE RESPONSE => ", responseImage);
+
+        const responseVideo = await videoUploadFileToCloudinary(videoFile, "aora");
+        console.log("VIDEO RESPONSE => ", responseVideo);
+
+
+
+        // database entry
+        console.log("DATABASE ENTRY CREATING");
+        const fileData = await Video.create({
+            title,
+            prompt,
+            user,
+            thumbnail: responseImage.secure_url,
+            videoUrl: responseVideo.secure_url
+        })
+        console.log("DATABASE ENTRY DONE");
+
+        res.json({
+            success: true,
+            message: "File created successfully created successfully"
+        })
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({
+            success: false,
+            message: "something went wrong while uploading image"
+        })
+    }
+}
+
+
+// video upload handler
+// exports.videoUpload = async (req, res) => {
+//     try {
+//         // fetch the data
+//         const { name, tags, email } = req.body;
+//         console.log(name, tags, email);
+
+//         // video file data
+//         const videoFile = req.files.video;
+//         console.log(videoFile)
+
+//         // validation
+//         const supportedTypes = ["mp4", "mp3", "mkv"];
+
+//         // current file type
+//         const currentFileType = videoFile.name.split(".")[1].toLowerCase();
+//         console.log("FILE EXTENSION : ", currentFileType);
+
+//         // file type not supported
+//         if (!isFileTypeSupported(currentFileType, supportedTypes)) {
+//             return res.status(400).json({
+//                 success: false,
+//                 message: "File type not supported"
+//             })
+//         }
+
+//         // file type is supported
+//         console.log("UPLOADING TO CLOUDINARY")
+//         const response = await videoUploadFileToCloudinary(videoFile, "dotbatch");
+//         // file = file name
+//         // dotbatch = folder name , it is in the cloudinary folder
+//         console.log("RESPONSE => ", response);
+
+//         // database entry
+//         console.log("DATABASE ENTRY CREATING");
+//         const fileData = await File.create({
+//             name,
+//             tags,
+//             email,
+//             fileUrl: response.url
+//         })
+//         console.log("DATABASE ENTRY DONE");
+
+//         res.json({
+//             success: true,
+//             message: "video created successfully created successfully"
+//         })
+//     } catch (e) {
+//         console.error("Something went wrong");
+//         res.status(400).json({
+//             success: false,
+//             message: "Something went wrong while uploading video"
+//         })
+//     }
+// }
+
+module.exports = videoFileUpload
